41279c5d9afe482d312df059f5303f692c1d9c0a
Saeedovic <Seedxlss@gmail.com>
2022-11-24 10:21:24 +0400

    edited the falling objects and fixed the spawns and delay

--- Assets/Scripts/Saeed/FallinBombs.cs
+++ Assets/Scripts/Saeed/FallinBombs.cs
@@ -12,7 +12,7 @@ namespace FruitKahoot
         {
             Vector3 position = transform.position;
             position.x = Random.Range(-9, 10);
-            speed = Random.Range(9, 20);
+            speed = Random.Range(6, 12);
             transform.position = position;
             SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
